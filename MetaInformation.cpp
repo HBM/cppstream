8c9a5c40f94fea900176b3c45702728b9d4034bf
mloy <matthias.loy@googlemail.com>
2014-07-22 11:16:40 +0200

    receive complete data block

--- MetaInformation.cpp
+++ MetaInformation.cpp
@@ -17,12 +17,15 @@ namespace hbm {
 		{
 			uint32_t typeBig;
 			socket.receiveComplete(&typeBig, sizeof(typeBig));
+
 			m_type = ntohl(typeBig);
 			if(m_type==METAINFORMATION_JSON) {
-				std::vector < char > data(size-sizeof(m_type));
-				socket.receive(&data[0], data.size());
+				size_t dataSize = size-sizeof(m_type);
+				std::vector < char > data(dataSize);
+				socket.receiveComplete(&data[0], data.size());
+				data.push_back('\0');
 
-				Json::Reader().parse(&data[0], &data[data.size()], m_jsonContent);
+				Json::Reader().parse(&data[0], &data[dataSize], m_jsonContent);
 			} else {
 				m_binaryContent.resize(size-sizeof(m_type));
 				socket.receive(&m_binaryContent[0], m_binaryContent.size());			}

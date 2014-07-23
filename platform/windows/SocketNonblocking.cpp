f1022a096a75246139ad0a52088e37f74ada0092
Matthias Loy <matthias.loy@hbm.com>
2014-07-23 12:50:16 +0200

    remove license header we need to put anoher one in here

--- platform/windows/SocketNonblocking.cpp
+++ platform/windows/SocketNonblocking.cpp
@@ -1,24 +1,3 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
-/*
- * Copyright (C) 2007 Hottinger Baldwin Messtechnik GmbH
- * Im Tiefen See 45
- * 64293 Darmstadt
- * Germany
- * http://www.hbm.com
- * All rights reserved
- *
- * The copyright to the computer program(s) herein is the property of
- * Hottinger Baldwin Messtechnik GmbH (HBM), Germany. The program(s)
- * may be used and/or copied only with the written permission of HBM
- * or in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
- * This copyright notice must not be removed.
- *
- * This Software is licenced by the
- * "General supply and license conditions for software"
- * which is part of the standard terms and conditions of sale from HBM.
-*/
-
 #include <stdint.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
@@ -27,10 +6,6 @@
 #undef min
 
 #define __time_t long
-#define syslog fprintf
-#define LOG_DEBUG stdout
-#define LOG_INFO stdout
-#define LOG_ERR stderr
 #define ssize_t int
 #define snprintf sprintf_s
 

694f18a92294a9b61af4a2c35179fd937ea49339
Matthias Loy <matthias.loy@hbm.com>
2014-10-29 14:43:20 +0100

    documentation

--- include/streamclient/deltatimeinfo.h
+++ include/streamclient/deltatimeinfo.h
@@ -8,17 +8,29 @@
 
 namespace hbm {
 	namespace streaming {
-		/// used for calculating NTP time stamps for measured values of synchronuous signals.
+		/// used for calculating the accumulated timestamp for measured values of synchronuous signals.
 		class deltaTimeInfo {
 		public:
 			deltaTimeInfo();
+			/// set accumulated timestamp
 			void setTime(const Json::Value& params);
 
+			/// set delta timestamp
 			/// \throws std::runtime_error if provided JSON is not valid
 			void setDelta(const Json::Value& params);
+
+			/// increase era, seconds, fraction and sub fraction by delta timestamp * count
+			/// \return the second and fraction part as NTP timestamp
+			/// the upper 32 byte are seconds
+			/// the lower 32 bytes are fraction of seconds (0.5, 0.25, 0.125...)
 			uint64_t increment(unsigned int count);
+
+			/// clear delta timestamp and accumulated timestamp
 			void clear();
 
+			/// \return the second and fraction part as NTP timestamp
+			/// the upper 32 byte are seconds
+			/// the lower 32 bytes are fraction of seconds (0.5, 0.25, 0.125...)
 			uint64_t ntpTimeStamp() const;
 		private:
 			/// may not be copied

5d9fbde9030b7b8fcf8f10a2cd3ee03c14fa1725
Matthias Loy <matthias.loy@hbm.com>
2014-10-29 14:26:44 +0100

    remove useless tests

--- test/teststreamclient.cpp
+++ test/teststreamclient.cpp
@@ -10,39 +10,11 @@
 #include <stdexcept>
 
 #include <boost/test/unit_test.hpp>
-#include <boost/thread/thread.hpp>
-#include <boost/bind.hpp>
 
 #include "streamclient/streamclient.h"
 #include "streamclient/timeinfo.h"
 #include "streamclient/deltatimeinfo.h"
 #include "teststreamclient.h"
-
-DeviceFixture::DeviceFixture()
-	: m_address("hbm-00087b")
-	, m_controlPort("http")
-	, m_streamClient()
-	, m_streamer(boost::thread(boost::bind(&hbm::streaming::StreamClient::start, &m_streamClient, m_address, hbm::streaming::DAQSTREAM_PORT, m_controlPort)))
-{
-}
-
-DeviceFixture::~DeviceFixture()
-{
-	m_streamClient.stop();
-	m_streamer.join();
-}
-
-BOOST_FIXTURE_TEST_SUITE(test, DeviceFixture)
-
-BOOST_AUTO_TEST_CASE (test_subscribe)
-{
-	boost::this_thread::sleep_for(boost::chrono::milliseconds(500));
-
-	hbm::streaming::signalReferences_t signalReferences;
-	signalReferences.push_back("aninvalidsignalreference");
-	BOOST_CHECK_THROW(m_streamClient.subscribe(signalReferences), std::runtime_error);
-	BOOST_CHECK_THROW(m_streamClient.unsubscribe(signalReferences), std::runtime_error);
-}
 
 
 BOOST_AUTO_TEST_CASE (test_timeInfo)
@@ -175,6 +147,3 @@ BOOST_AUTO_TEST_CASE (test_deltaTimeInfo)
 	timestamp = deltaTime.ntpTimeStamp();
 	BOOST_CHECK(timestamp==(0x0200000000));
 }
-
-
-BOOST_AUTO_TEST_SUITE_END()

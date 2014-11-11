cc2dbb76dfea5a385a119a0e2743c80d3ca4f0ef
mloy <matthias.loy@googlemail.com>
2014-11-11 10:31:31 +0100

    process meta information internally before calling callback function. subscribedSignal does know its signal number

--- include/streamclient/subscribedsignal.h
+++ include/streamclient/subscribedsignal.h
@@ -20,7 +20,7 @@ namespace hbm {
 		/// \warning for windows, this class works on little endian machines only. this is because of endian issues.
 		class SubscribedSignal {
 		public:
-			SubscribedSignal();
+			SubscribedSignal(unsigned int signalNumber);
 
 			/// process measured data
 			/// \return number of bytes processed
@@ -29,9 +29,14 @@ namespace hbm {
 			/// process signal related meta information.
 			void processSignalMetaInformation(const std::string& method, const Json::Value& params);
 
-			std::string signalReference()
+			std::string signalReference() const
 			{
 				return m_signalReference;
+			}
+
+			unsigned int signalNumber() const
+			{
+				return m_signalNumber;
 			}
 
 		private:
@@ -70,6 +75,7 @@ namespace hbm {
 				TIMETYPE_NTP
 			};
 
+			unsigned int m_signalNumber;
 			std::string m_signalReference;
 
 			/// For synchronuous signals (Pattern V): will be set to the time of the first measured value and will be incremented with each value received.
